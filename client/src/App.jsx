072f530428f86c7f88608f9cd88f10013ba3baaf
Fay4z <fayazjr212@gmail.com>
2024-03-29 21:26:44 +0530

    initial setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <p className=" text-3xl font-bold">HIII</p>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
